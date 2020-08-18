811b0e951eb694cae63d972c1d4ddbe71c7d32f1
yuuta1 <oozoranohonoo@yahoo.co.jp>
2020-08-18 19:57:38 +0900

    first commit

---
+++ src/views/Login.vue
@@ -0,0 +1,60 @@
+<template>
+ <div>
+   <HeaderAuth />
+   <div class="card">
+     <p>ログイン</p>
+     <div class="form">
+       <input placeholder="メールアドレス" type="email" />
+       <input placeholder="パスワード" type="password" />
+       <button>ログイン</button>
+     </div>
+   </div>
+ </div>
+</template>
+
+<script>
+import HeaderAuth from "../components/HeaderAuth";
+export default {
+ components: {
+   HeaderAuth
+ }
+};
+</script>
+
+<style scoped>
+button {
+ width: 100px;
+ text-align: center;
+ padding: 8px 0 10px;
+ color: #fff;
+ background-color: #5419da;
+ border-radius: 25px;
+ cursor: pointer;
+}
+.card {
+ margin: 100px auto;
+ width: 350px;
+ background: #fff;
+ border-radius: 5px;
+ padding: 20px;
+}
+.card p {
+ color: black;
+ font-weight: bold;
+ text-align: center;
+}
+input {
+ margin-top: 15px;
+ width: 80%;
+ border-radius: 10px;
+ padding: 10px;
+ border: 1px solid black;
+ color: black;
+}
+.form {
+ text-align: center;
+}
+.form button {
+ margin-top: 15px;
+}
+</style>
